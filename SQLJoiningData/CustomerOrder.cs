2f78e15e1475b6185aa3ce6a445a3a9dd41f1ccb
Troy <wilson.troy.anthony@gmail.com>
2022-08-29 20:55:29 -0400

    Add project files.

---
+++ SQLJoiningData/CustomerOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLJoiningData
+{
+    internal class CustomerOrder
+    {
+        public string CustomerName { get; set; }
+        public string Item { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public CustomerOrder(string customerName, string item, decimal price, int quantity)
+        {
+            CustomerName = customerName;
+            Item = item;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ SQLJoiningData/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace SQLJoiningData
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<CustomerOrder> customerOrders = new() 
+            {
+                new CustomerOrder("Acme Hardware", "Mouse", 25m, 3),
+                new CustomerOrder("Acme Hardware", "Keyboard", 45m, 2),
+                new CustomerOrder("Falls Realty", "Macbook", 800m, 2),
+                new CustomerOrder("Julie’s Morning Diner", "iPad", 525m, 1),
+                new CustomerOrder("Julie’s Morning Diner", "Credit Card Reader", 45m, 1),
+            };
+
+            var uniqueCustomers = customerOrders.Select(c => c.CustomerName).Distinct();
+
+            foreach(var customer in uniqueCustomers)
+            {
+                Console.WriteLine(customer);
+                Console.WriteLine("\t{0,-21}{1,-9}{2,-10}{3}", "Item", "Price", "Quantity", "Total");
+                foreach(var order in customerOrders.Where(c => c.CustomerName == customer))
+                {
+                    Console.WriteLine($"\t{order.Item,-20} {order.Price,-8} {order.Quantity,-10}{order.Price * order.Quantity}");
+                }
+                Console.WriteLine();
+            }
+
+
+            foreach(var customer in uniqueCustomers)
+            {
+                Decimal total = 0;
+                Console.WriteLine(customer);
+                Console.WriteLine("\t{0,-21}{1,-9}{2,-10}{3}", "Item", "Price", "Quantity", "Total");
+                foreach(var order in customerOrders.Where(c => c.CustomerName == customer))
+                {
+                    total += order.Price * order.Quantity;
+                    Console.WriteLine($"\t{order.Item,-20} {order.Price,-8} {order.Quantity,-10}{order.Price * order.Quantity}");
+                }
+                Console.WriteLine($"Total{total,46}");
+                Console.WriteLine();
+            }
+
+            foreach(var customer in uniqueCustomers)
+            {
+                Decimal total = 0;
+                int itemCount = 0;
+                Console.WriteLine("{0,-25}{1,-21}{2,-9}{3,-10}{4}","Customer", "Item", "Price", "Quantity", "Total");
+                foreach(var order in customerOrders.Where(c => c.CustomerName == customer))
+                {
+                    total += order.Price * order.Quantity;
+                    if(itemCount==0)
+                    {
+                        Console.WriteLine($"{order.CustomerName,-25}{order.Item,-20} {order.Price,-8} {order.Quantity,-10}{order.Price * order.Quantity}");
+                        itemCount++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{"",-25}{order.Item,-20} {order.Price,-8} {order.Quantity,-10}{order.Price * order.Quantity}");
+                    }
+                }
+                Console.WriteLine($"Total{total,63}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
